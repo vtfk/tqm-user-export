20f9ac2c2a0a2f8fa531d60e747bda48c6105fd0
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2023-12-11 15:56:21 +0100

    added full path to xml-path

--- Start-UserExport.ps1
+++ Start-UserExport.ps1
@@ -93,7 +93,7 @@ if ($papertrailConfig) {
 Add-LogTarget -Name Teams -Configuration @{ WebHook = $tqmConfig.TeamsWebhook; Level = "ERROR" }
 
 # file paths
-$xmlFolder = ".\logs"
+$xmlFolder = "$PSScriptRoot\logs"
 $xmlPath = "$xmlFolder\export_$((Get-Date).DayOfWeek.ToString().ToLower()).xml"
 $logPath = "$xmlFolder\tqm_$((Get-Date).DayOfWeek.ToString().ToLower()).log"
 $sftpPath = "\$($tqmConfig.Firmakode)-$(Get-Date -Format 'yyyyMMddHHmmss').xml"
